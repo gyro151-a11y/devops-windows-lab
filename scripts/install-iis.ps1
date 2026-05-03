31cc6c27946e34c36bc9fc52415414a4a5c3b9e6
Jeffrey Edgar <gyro151@gmail.com>
2026-05-03 16:15:43 -0500

    Update webpage to test pipeline success

--- scripts/install-iis.ps1
+++ scripts/install-iis.ps1
@@ -1,7 +1,7 @@
 Install-WindowsFeature -Name Web-Server -IncludeManagementTools
 
 # Create a simple webpage
-$html = '<h1>Hello from Terraform + Script File</h1>'
+$html = '<h1>Congratulations! You have a working CI/CD pipeline!</h1>'
 $html | Out-File -FilePath "C:\inetpub\wwwroot\index.html" -Encoding utf8
 
 iisreset
